4dfc6b61083af2db5f7104fb5e3656897bdb0181
saracassandra7 <saracassandra7@gmail.com>
2022-10-10 16:09:38 +0200

    [add] js password generator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('ciao');
+//dichiarazione delle variabili
+const nomeUtente = prompt('qual è il tuo nome?');
+const cognomeUtente = prompt('qual è il tuo cognome?');
+const colorePreferito = prompt('qual è il tuo colore preferito?');
+
+//output
+document.getElementById('pwd').innerHTML += `
+ ${nomeUtente}${cognomeUtente}${colorePreferito}22
+`;
+
+
